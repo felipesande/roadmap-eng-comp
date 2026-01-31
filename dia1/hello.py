b8602d0db1fca0f0ff92ef51ed6102fa202aee50
Felipe Sande <felipe.sande.eng@gmail.com>
2026-01-31 18:27:57 -0300

    Dia 1: ambiente, Python, Git e primeiros scripts

---
+++ dia1/hello.py
@@ -0,0 +1,13 @@
+nome = "Felipe"
+idade = 21
+altura = 1.75
+focado_em_ia = True
+print(nome)
+print(idade)
+print(altura)
+print(focado_em_ia)
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(focado_em_ia))
+print(nome + " tem " + str(idade) + " anos")
