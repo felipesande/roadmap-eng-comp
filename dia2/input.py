1e77111d0d136a402cdfe8fe27e751f9bf7f02f3
Felipe Sande <felipe.sande.eng@gmail.com>
2026-02-01 14:41:26 -0300

    Dia 2: input e operadores matemáticos

---
+++ dia2/input.py
@@ -0,0 +1,16 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade quando fizer aniversário esse ano: "))
+"""
+print("Ano que vem você terá", idade + 1)
+a = 10
+b = 3
+print(a + b) # soma
+print(a - b) # subtração
+print(a * b) # multiplicação
+print(a / b) # divisão
+print(a // b) # divisão inteira
+print(a % b) # resto
+print(a ** b) # potência
+"""
+nasceu = 2026 - idade
+print(nome + ", você nasceu em", nasceu)
